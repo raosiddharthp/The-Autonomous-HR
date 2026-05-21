8db8955a24e0e439fae2c5f2dcd38bc729926ef4
Siddharth Rao Potukuchi <info@raosiddharth.com>
2026-05-21 13:42:12 +0530

    feat: add twilio-auth-token and twilio-phone-number secrets (S-04)

--- infra/terraform/secrets.tf
+++ infra/terraform/secrets.tf
@@ -21,3 +21,19 @@ resource "google_secret_manager_secret" "twilio_sid" {
     auto {}
   }
 }
+
+resource "google_secret_manager_secret" "twilio_auth_token" {
+  secret_id = "twilio-auth-token"
+  project   = var.project_id
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret" "twilio_phone_number" {
+  secret_id = "twilio-phone-number"
+  project   = var.project_id
+  replication {
+    auto {}
+  }
+}
